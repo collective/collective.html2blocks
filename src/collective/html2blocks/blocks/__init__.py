87790c6565239487e6c63af12130faba0839afe8
Érico Andrei <ericof@gmail.com>
2025-04-15 11:14:01 -0300

    Expose iframe_block

--- src/collective/html2blocks/blocks/__init__.py
+++ src/collective/html2blocks/blocks/__init__.py
@@ -1,3 +1,4 @@
+from .iframe import iframe_block
 from .image import image_block
 from .slate import slate_block
 from .table import table_block
@@ -5,6 +6,7 @@ from .video import video_block
 
 
 __all__ = [
+    "iframe_block",
     "image_block",
     "slate_block",
     "table_block",
